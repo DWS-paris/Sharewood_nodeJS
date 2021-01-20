310dc90fe43334b4aa92276178edddba6ecf4c7e
Julien Noyer <j.noyer@digitalworkshop.fr>
2021-01-20 10:20:44 +0100

    Création du ficheir server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+/* 
+Imports
+*/
+    // NPM modules
+    require('dotenv').config(); //=> https://www.npmjs.com/package/dotenv
+    const express = require('express'); //=> https://www.npmjs.com/package/express
+    const bodyParser = require('body-parser'); //=> https://www.npmjs.com/package/body-parser
+    const cookieParser = require('cookie-parser'); //=> https://www.npmjs.com/package/cookie-parser
+    const passport = require('passport'); //=> https://www.npmjs.com/package/passport
+    const path = require('path'); //=> https://www.npmjs.com/package/path
+//
