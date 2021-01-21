3e82af6460fa18c96f1734aca0b1fd92ba68e0b7
Julien Noyer <j.noyer@digitalworkshop.fr>
2021-01-21 10:50:47 +0100

    Mise en place de la route BACKEND register

--- routers/backend.router.js
+++ routers/backend.router.js
@@ -3,6 +3,11 @@ Imports
 */
     // Node
     const express = require('express');
+
+    // Inner
+    const Controllers = require('../controllers/index')
+    const Mandatory = require('../services/mandatory.service');
+    const { checkFields } = require('../services/request.service');
 //
 
 /* 
@@ -18,6 +23,31 @@ Router definition
             this.router.get('/', (req, res) => {
                 // Return "index.ejs" file
                 return res.render('index')
+            })
+
+            // Define index route
+            this.router.post('/register', (req, res) => {
+                // Check body data
+                if( typeof req.body === 'undefined' || req.body === null || Object.keys(req.body).length === 0 ){ 
+                    return sendBodyError(`/register`, 'POST', res, 'No data provided in the reqest body')
+                }
+                else{
+                    // Use the controller to create data
+                    const { ok, extra, miss } = checkFields( Mandatory.register, req.body )
+
+                    // Error: bad fields provided
+                    if( !ok ){ return sendFieldsError(`/register`, 'POST', res, 'Bad fields provided', miss, extra) }
+                    else{
+                        // Create new object
+                        Controllers.user.register(req)
+                        .then( apiResponse => {
+                            res.render( 'index', { data: apiResponse, err: null } )
+                        })
+                        .catch( apiError => {
+                            res.render( 'index', { data: null, err: apiError } )
+                        });
+                    }
+                }
             })
         }
 
